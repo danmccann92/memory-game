28edea796e9204a179215b52541ad3a26af02cb1
Dan McCann <danmccann10@gmail.com>
2018-09-01 16:50:00 -0400

    first commit, utilizing Udacity's starter code for time constraints

---
+++ js/app.js
@@ -0,0 +1,49 @@
+/*
+ * Create a list that holds all of your cards
+ */
+var cardList = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o', 'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube',  'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle',  'fa-bomb', 'fa-bomb'];
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = cardList.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = cardList[currentIndex];
+        cardList[currentIndex] = cardList[randomIndex];
+        cardList[randomIndex] = temporaryValue;
+    }
+
+    return cardList;
+}
+shuffle();
+
+
+function generateCards (cards) {
+  for (i = 0; i < cardList.length; i++) {
+    var cardValue = document.getElementsByClassName('.fa').classList.add += cardList[i];
+    return cardValue;
+};
+};
+assignCardValue();
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+
+/* Shuffle function I was trying to develop on my own */
